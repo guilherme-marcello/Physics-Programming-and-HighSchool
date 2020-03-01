17fdcc93a56b6dcca56098f10afefbd20e050e1b
guilherme-marcelo <58813280+guilherme-marcelo@users.noreply.github.com>
2020-03-01 19:37:46 +0000

    Update operacoes.py

--- Pontape-Inicial/Variaveis/operacoes.py
+++ Pontape-Inicial/Variaveis/operacoes.py
@@ -1,8 +1,9 @@
 diasdasemana = 7
 
-print(7+diasdasemana)  # 14
-print(1-diasdasemana)  # -6 
-print(3*diasdasemana)  # 21
-print(21/diasdasemana) # 3.0
-print(diasdasemana**2) # 49
-print(12%diasdasemana) # 5
+print(7+diasdasemana)   # 14
+print(1-diasdasemana)   # -6 
+print(3*diasdasemana)   # 21
+print(21/diasdasemana)  # 3.0
+print(diasdasemana**2)  # 49
+print(12%diasdasemana)  # 5
+print(12//diasdasemana) # 1

02a32529f31c15f8c652f75ba8020814aa47b12f
guilherme-marcelo <58813280+guilherme-marcelo@users.noreply.github.com>
2020-03-01 11:26:17 +0000

    Create operacoes.py

---
+++ Pontape-Inicial/Variaveis/operacoes.py
@@ -0,0 +1,8 @@
+diasdasemana = 7
+
+print(7+diasdasemana)  # 14
+print(1-diasdasemana)  # -6 
+print(3*diasdasemana)  # 21
+print(21/diasdasemana) # 3.0
+print(diasdasemana**2) # 49
+print(12%diasdasemana) # 5

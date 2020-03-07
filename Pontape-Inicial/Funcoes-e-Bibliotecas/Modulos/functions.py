e5dd15cad9b81843c964f72e6f6b991a6cb79f01
guilherme-marcello <58813280+guilherme-marcello@users.noreply.github.com>
2020-03-07 21:04:57 +0000

    Create functions.py

---
+++ Pontape-Inicial/Funcoes-e-Bibliotecas/Modulos/functions.py
@@ -0,0 +1,11 @@
+def get_f(x):
+    f = x**2 + 10
+    return f
+
+def superprint(string):
+    print('_'*len(string))
+    print('\n'+string+'\n')
+    print('_'*len(string))
+    
+def get_hipotenusa(c1,c2):
+    return (c1**2 + c2**2)**(.5)

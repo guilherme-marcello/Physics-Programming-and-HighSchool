c83978399efe5cff8a620a671b40e3792fa275c6
guilherme-marcello <58813280+guilherme-marcello@users.noreply.github.com>
2020-03-07 15:27:52 +0000

    Update logicos.py

--- Pontape-Inicial/Variaveis/Operadores/logicos.py
+++ Pontape-Inicial/Variaveis/Operadores/logicos.py
@@ -10,3 +10,29 @@ h = c or d
 
 #not
 i = not f
+
+#if: se p então q
+if 2>1:
+  print("2 é maior que 1")
+  
+#elif: se não p, portanto se p2 então q2
+if 8<2:
+  print("10<2")
+elif 8%2 == 0:
+  print("8 é par")
+  
+#else
+if "n"=="o":
+  a = "n"+"o"
+else:
+  a = "n"
+
+#elif alternativo
+if 8<2:
+    print("2 é maior que 8")
+else:
+    if 8%2 == 0:
+        print("8 é par")
+
+
+
